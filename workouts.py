4c2abeedff672fa87ff14a2bb12dd2c5627a95fe
alanchrissantony <alanchrissantony@gmail.com>
2021-04-21 22:30:47 +0530

    2021-04-21T22:30:47+05:30

--- workouts.py
+++ workouts.py
@@ -5098,3 +5098,27 @@ def has_exactly_three_divisors(n: int) -> bool:
     """
     count = sum(1 for i in range(1, n + 1) if n % i == 0)
     return count == 3
+
+
+# --- Next Function Block ---
+
+
+
+def check_reordering(nums: list) -> bool:
+    """
+    Attempts a reordering based on the provided logic and returns True if the concatenation of 
+    modified segments equals the sorted original list.
+    (The logic is directly derived from the snippet and might be non-standard.)
+    """
+    original_sorted = sorted(nums)
+    arr = []
+    i = 0
+    nums_copy = nums[:]
+    while i < len(nums_copy) - 1:
+        if nums_copy[i + 1] < nums_copy[i]:
+            j = i + 1
+            while j < len(nums_copy):
+                arr.append(nums_copy[j])
+                del nums_copy[j]
+        i += 1
+    return (arr + nums_copy) == original_sorted
