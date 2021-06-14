4e6861c77b1b30fcc30b2caa9e08c45a2e64b1bb
alanchrissantony <alanchrissantony@gmail.com>
2021-06-14 22:31:11 +0530

    2021-06-14T22:31:11+05:30

--- workouts.py
+++ workouts.py
@@ -6996,3 +6996,22 @@ def extract_number(s: str) -> int:
         except ValueError:
             continue
     return 0
+
+
+# --- Next Function Block ---
+
+
+
+def longest_common_prefix_brute(strs: list) -> str:
+    """
+    Returns the longest common prefix among the given strings using a brute-force method.
+    """
+    if not strs:
+        return ""
+    prefix = strs[0]
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if prefix == "":
+                return ""
+    return prefix
