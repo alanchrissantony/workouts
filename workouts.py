4e4b0a68a29ce46e31270ea212423924b23737fa
alanchrissantony <alanchrissantony@gmail.com>
2021-06-25 22:29:51 +0530

    2021-06-25T22:29:51+05:30

--- workouts.py
+++ workouts.py
@@ -781,3 +781,16 @@ def h(n):
             else:
                 print(end=" ")
         print("\n")
+
+
+# --- Next Function Block ---
+
+
+def i(n):
+    for i in range(1, n+1):
+        for j in range(0, n+1):
+            if j<i:
+                print(end="  ")
+            else:
+                print(" *", end="")
+        print("\n")
