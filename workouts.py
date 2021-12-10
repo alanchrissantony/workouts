dfbe9423b06e9f1f233e3cfc1b92195ba16c439a
alanchrissantony <alanchrissantony@gmail.com>
2021-12-10 22:33:36 +0530

    2021-12-10T22:33:36+05:30

--- workouts.py
+++ workouts.py
@@ -21571,3 +21571,14 @@ def binary_ones_counts(n: int) -> list:
         counts.append(bin(i)[2:].count("1"))
         i += 1
     return counts
+
+
+# --- Next Function Block ---
+
+
+# --- XOR Elements ---
+def xor_elements(nums: list, k: int) -> list:
+    """
+    Returns a list containing the result of XORing each element of nums with k.
+    """
+    return [num ^ k for num in nums]
