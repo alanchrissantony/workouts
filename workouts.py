9c9fdcb2f81aa98bf871e4d2180823a2d626b5d4
alanchrissantony <alanchrissantony@gmail.com>
2022-01-02 22:31:04 +0530

    2022-01-02T22:31:04+05:30

--- workouts.py
+++ workouts.py
@@ -6422,3 +6422,21 @@ def normalize_emails(emails: list) -> list:
         local = local.split('+')[0].replace('.', '')
         return local + '@' + domain
     return [normalize(email) for email in emails]
+
+
+# --- Next Function Block ---
+
+
+
+def adjust_prices(prices: list) -> list:
+    """
+    For each price in prices, finds the first subsequent smaller price and subtracts it from the current price.
+    Returns the adjusted list.
+    """
+    adjusted = prices[:]
+    for i in range(len(adjusted) - 1):
+        for j in range(i + 1, len(adjusted)):
+            if adjusted[i] > adjusted[j]:
+                adjusted[i] -= adjusted[j]
+                break
+    return adjusted
