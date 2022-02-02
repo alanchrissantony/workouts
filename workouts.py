01a93f659e3d6acd65c304577598af0fd21fb7eb
alanchrissantony <alanchrissantony@gmail.com>
2022-02-02 22:30:20 +0530

    2022-02-02T22:30:20+05:30

--- workouts.py
+++ workouts.py
@@ -3086,3 +3086,14 @@ def count_equal_pairs(nums: list) -> int:
             if nums[i] == nums[j]:
                 count += 1
     return count
+
+
+# --- Next Function Block ---
+
+
+
+def duplicate_list(nums: list) -> list:
+    """
+    Returns a new list by appending a copy of nums to itself.
+    """
+    return nums + nums
