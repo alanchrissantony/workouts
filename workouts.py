9f312211a4b84001578566f2587ed0ccd88dc1ee
alanchrissantony <alanchrissantony@gmail.com>
2022-03-18 22:30:30 +0530

    2022-03-18T22:30:30+05:30

--- workouts.py
+++ workouts.py
@@ -3802,3 +3802,21 @@ def reconstruct_from_pieces(arr: list, pieces: list) -> (bool, list):
             used.append(piece)
             collected.extend(piece)
     return (collected == arr, collected)
+
+
+# --- Next Function Block ---
+
+
+
+def is_arithmetic_progression(arr: list) -> bool:
+    """
+    Checks if the sorted array forms an arithmetic progression.
+    """
+    arr = sorted(arr)
+    if len(arr) < 2:
+        return True
+    diff = arr[1] - arr[0]
+    for i in range(1, len(arr) - 1):
+        if arr[i + 1] - arr[i] != diff:
+            return False
+    return True
