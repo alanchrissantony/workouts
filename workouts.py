00691e2b0b81a62a465fedd31061c46015d7247f
alanchrissantony <alanchrissantony@gmail.com>
2022-03-28 22:32:16 +0530

    2022-03-28T22:32:16+05:30

--- workouts.py
+++ workouts.py
@@ -12727,3 +12727,18 @@ def remove_digit(number: str, digit: str) -> (str, list):
         else:
             result += ch
     return result, indices
+
+
+# --- Next Function Block ---
+
+
+
+def are_frequencies_unique(nums: list) -> bool:
+    """
+    Returns True if the frequency of each number in nums is unique.
+    """
+    freq = {}
+    for num in nums:
+        freq[num] = freq.get(num, 0) + 1
+    freqs = list(freq.values())
+    return len(freqs) == len(set(freqs))
