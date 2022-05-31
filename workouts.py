6ec5c76b421477d1d613ac72a91693b6787d9992
alanchrissantony <alanchrissantony@gmail.com>
2022-05-31 22:30:26 +0530

    2022-05-31T22:30:26+05:30

--- workouts.py
+++ workouts.py
@@ -3601,3 +3601,18 @@ def capitalize_first_letter(s: str) -> str:
     if not s:
         return s
     return s[0].upper() + s[1:]
+
+
+# --- Next Function Block ---
+
+
+
+def interleave_halves(nums: list, n: int) -> list:
+    """
+    Given a list nums and an integer n, interleaves the first n elements with the next n elements.
+    """
+    result = []
+    for i in range(n):
+        result.append(nums[i])
+        result.append(nums[n + i])
+    return result
