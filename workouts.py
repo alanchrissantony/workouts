5d72023514d5d5f152a1f749ce1a4c84fe1b8f1e
alanchrissantony <alanchrissantony@gmail.com>
2022-12-19 22:32:37 +0530

    2022-12-19T22:32:37+05:30

--- workouts.py
+++ workouts.py
@@ -14607,3 +14607,18 @@ def remove_duplicates(nums: list) -> int:
             j += 1
             nums[j] = nums[i]
     return j + 1
+
+
+# --- Next Function Block ---
+
+
+
+def normalize_emails(emails: list) -> list:
+    """
+    Normalizes emails by removing dots in the local part and ignoring characters after '+'.
+    """
+    def normalize(email: str) -> str:
+        local, domain = email.split('@')
+        local = local.split('+')[0].replace('.', '')
+        return local + '@' + domain
+    return [normalize(email) for email in emails]
