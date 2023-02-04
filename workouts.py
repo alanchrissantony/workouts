a5edfebea22ed87ecd9c9c26d369d9ae4c03ca88
alanchrissantony <alanchrissantony@gmail.com>
2023-02-04 22:33:17 +0530

    2023-02-04T22:33:17+05:30

--- workouts.py
+++ workouts.py
@@ -18625,3 +18625,24 @@ def elimination_game(n: int) -> int:
         arr.reverse()
         reverse = not reverse
     return arr[0]
+
+
+# --- Next Function Block ---
+
+
+# 12. Summary ranges: given a sorted list, return summary ranges.
+def summary_ranges(nums: list) -> list:
+    ranges = []
+    i = 0
+    n = len(nums)
+    while i < n:
+        start = nums[i]
+        while i + 1 < n and nums[i+1] == nums[i] + 1:
+            i += 1
+        end = nums[i]
+        if start == end:
+            ranges.append(str(start))
+        else:
+            ranges.append(f"{start}->{end}")
+        i += 1
+    return ranges
