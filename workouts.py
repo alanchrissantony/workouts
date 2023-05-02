39918566af0b528ed9c3ca85fc48516f10eb9aaa
alanchrissantony <alanchrissantony@gmail.com>
2023-05-02 22:32:01 +0530

    2023-05-02T22:32:01+05:30

--- workouts.py
+++ workouts.py
@@ -11455,3 +11455,20 @@ def max_gap_between_repeats(s: str) -> int:
             if s[i] == s[j]:
                 gaps.append(j - i - 1)
     return max(gaps) if gaps else -1
+
+
+# --- Next Function Block ---
+
+
+
+def first_duplicate(nums: list):
+    """
+    Returns the first duplicate element found in nums.
+    If no duplicate exists, returns None.
+    """
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return num
+        seen.add(num)
+    return None
