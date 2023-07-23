0ceffaa7e76c628c3d2842c6c4b5d158c272b195
alanchrissantony <alanchrissantony@gmail.com>
2023-07-23 22:33:54 +0530

    2023-07-23T22:33:54+05:30

--- workouts.py
+++ workouts.py
@@ -24331,3 +24331,30 @@ def to_base7(num: int) -> str:
         digits.append(str(num % 7))
         num //= 7
     return "".join(digits[::-1])
+
+
+# --- Next Function Block ---
+
+
+# -----------------------------------------------------
+# Function 9: Custom binary search.
+def binary_search(nums: list, target: int) -> int:
+    """
+    Performs a binary search on a sorted list 'nums' to find 'target'.
+    Returns the index if found, or -1 if not found.
+    Uses a simple iterative method.
+    
+    Example:
+      nums = [-1,0,3,5,9,12], target = 2 → returns -1 (target not found)
+    """
+    start = 0
+    end = len(nums) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return -1
