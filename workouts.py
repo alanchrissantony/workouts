7aad375206f8cc8666ca27490bfa97a709f3475e
alanchrissantony <alanchrissantony@gmail.com>
2023-11-10 22:30:37 +0530

    2023-11-10T22:30:37+05:30

--- workouts.py
+++ workouts.py
@@ -4364,3 +4364,14 @@ def custom_sum(n: int) -> int:
         total += count + pos
         count += 1
     return total
+
+
+# --- Next Function Block ---
+
+
+
+def index_based_mapping(nums: list) -> list:
+    """
+    Creates a new list where each element is replaced by the element at the index equal to its value.
+    """
+    return [nums[nums[i]] for i in range(len(nums))]
