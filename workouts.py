a612dbd04d163288bec6d3ff398629cec545ebcb
alanchrissantony <alanchrissantony@gmail.com>
2023-12-05 22:30:33 +0530

    2023-12-05T22:30:33+05:30

--- workouts.py
+++ workouts.py
@@ -4066,3 +4066,14 @@ def first_multiple_in_array(n: int, arr: list) -> int:
         if i * n in arr:
             return i * n
     return None
+
+
+# --- Next Function Block ---
+
+
+
+def generate_even_numbers(limit: int) -> list:
+    """
+    Returns a list of even numbers: 2, 4, 6, ... up to limit*2.
+    """
+    return [i * 2 for i in range(1, limit + 1)]
