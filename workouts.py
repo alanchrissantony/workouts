53c99c95576aa66703add7e0e52273403516c507
alanchrissantony <alanchrissantony@gmail.com>
2024-02-12 22:32:36 +0530

    2024-02-12T22:32:36+05:30

--- workouts.py
+++ workouts.py
@@ -14526,3 +14526,16 @@ def first_duplicate(nums: list):
             return num
         seen.add(num)
     return None
+
+
+# --- Next Function Block ---
+
+
+
+def capitalize_first_letter(s: str) -> str:
+    """
+    Returns the string s with its first letter capitalized.
+    """
+    if not s:
+        return s
+    return s[0].upper() + s[1:]
