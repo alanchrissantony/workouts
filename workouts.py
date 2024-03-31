0695d4dff48b2f8b551cf2221c5fb999a0904cc6
alanchrissantony <alanchrissantony@gmail.com>
2024-03-31 22:33:33 +0530

    2024-03-31T22:33:33+05:30

--- workouts.py
+++ workouts.py
@@ -21094,3 +21094,28 @@ def remove_triplet_with_sum(arr: list) -> list:
                 j += 1
         i += 1
     return arr
+
+
+# --- Next Function Block ---
+
+
+# --- Max Remaining Money After Purchase ---
+def max_remaining_money(prices: list, money: int) -> int:
+    """
+    Given a list of prices and a total amount of money, finds two prices whose sum is maximized
+    without exceeding money. Returns the remaining money (money - sum) if such a pair exists,
+    otherwise returns money.
+    """
+    val = 0
+    flag = False
+    i = 0
+    while i < len(prices) - 1:
+        j = i + 1
+        while j < len(prices):
+            s = prices[i] + prices[j]
+            if money - s >= 0 and money - s > val:
+                val = money - s
+                flag = True
+            j += 1
+        i += 1
+    return val if flag else money
