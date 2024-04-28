7e278d3113d0c5e5fbae27a27420d1ac43fe322e
alanchrissantony <alanchrissantony@gmail.com>
2024-04-28 22:32:47 +0530

    2024-04-28T22:32:47+05:30

--- workouts.py
+++ workouts.py
@@ -15572,3 +15572,21 @@ def adjust_prices(prices: list) -> list:
                 adjusted[i] -= adjusted[j]
                 break
     return adjusted
+
+
+# --- Next Function Block ---
+
+
+
+def sum_diagonals(mat: list) -> int:
+    """
+    Returns the sum of the primary and secondary diagonals of a square matrix.
+    If the matrix size is odd, the center element is counted only once.
+    """
+    n = len(mat)
+    total = 0
+    for i in range(n):
+        total += mat[i][i] + mat[i][n - 1 - i]
+    if n % 2 == 1:
+        total -= mat[n // 2][n // 2]
+    return total
