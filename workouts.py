9bbf5834f8e2e8600fdb2883b82aae0385d9f49e
alanchrissantony <alanchrissantony@gmail.com>
2025-01-25 22:30:53 +0530

    2025-01-25T22:30:53+05:30

--- workouts.py
+++ workouts.py
@@ -5515,3 +5515,14 @@ def are_frequencies_unique(nums: list) -> bool:
         freq[num] = freq.get(num, 0) + 1
     freqs = list(freq.values())
     return len(freqs) == len(set(freqs))
+
+
+# --- Next Function Block ---
+
+
+
+def max_digit_count(s: str) -> int:
+    """
+    Returns the maximum count between occurrences of '1' and '0' in s.
+    """
+    return max(s.count("1"), s.count("0"))
